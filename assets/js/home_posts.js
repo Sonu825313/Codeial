10d475d2ab20ccc71548c4311a7044f95d54f553
Sonu825313 <sonukumarraj1341998@gmail.com>
2022-07-03 19:00:34 +0530

    converting to ajax creating a post:deleting a post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
             success: function(data){
                 let newPost = newPostDom(data.data.post);
                 $('#posts-list-container>ul').prepend(newPost);
+                deletePost($(' .delete-post-button', newPost));
             }, error: function(error){
                console.log(error.responseText);
             }
@@ -23,37 +24,57 @@
     //method to create a post in DOM
 
     let newPostDom = function(post){
-      return $(`<li id = "post-${post._id }">
+      return $(`<li id="post-${post._id}">
       <p>
-        
-              <small>
-                 <a class="delete post-button" href="/posts/destroy/${ post.id }"> X </a>
-              </small>
+      
             
-              ${post.content }
+        <small>
+            <a class="delete-post-button" href="/posts/destroy/${post._id}"> X </a>
+        </small>
+     
+          ${post.content}
           <br>
           <small>
-          ${ post.user.name }
+              ${post.user.name} 
           </small>
-          <div class="post-comments">
-             
-                  <form action="/comments/create" method="POST">
-                      <input type="text" name="content" placeholder="Type here to add comments..." required>
-                      <input type="hidden" name="post" value="${ post._id }">
-                      <input type="submit" value="Add Comments">
-                  </form>
-               
-                  <div class="post-comments-list"> 
-                      <ul id="post-comments-${post.id }">
-                       
-                      </ul>
-                  </div>
-          </div>
       </p>
-    
+      <div class="post-comments">
+        
+            <form action="/comments/create" method="POST">
+                <input type="text" name="content" placeholder="Type here to Comments..." required>
+                <input type="hidden" name="post" value="${post._id}" >
+                <input type="submit" value="Add Comment">
+            </form>
+  
+      
+  
+        <div class="post-comments-list">
+            <ul id="post-comments-${post._id}">
+              
+            </ul>
+        </div>
+      </div>
+      
   </li>`)
-    }
+  }
 
-    createPost();
-       
+  // method to delete a post from DOM
+  let deletePost = function(deleteLink){
+    $(deleteLink).click(function(e){
+        e.preventDefault();
+
+        $.ajax({
+            type: 'get',
+            url: $(deleteLink).prop('href'),
+            success: function(data){
+                $(`#post-${data.data.post_id}`).remove();
+            },error: function(error){
+                console.log(error.responseText);
+            }
+        });
+    });
+}
+
+
+  createPost();
 }
