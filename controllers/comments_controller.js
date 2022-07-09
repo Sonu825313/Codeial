aad9b0f3a3c7f94e7d9374a315806a40aaec42b6
Sonu825313 <sonukumarraj1341998@gmail.com>
2022-07-09 09:51:09 +0530

    playing with api

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -12,13 +12,30 @@ module.exports.create = async function(req,res){
           });
                 post.comments.push(comment);
                 post.save();
+          
+                if (req.xhr){
+                    // Similar for comments to fetch the user's id!
+                    comment = await comment.populate('user', 'name').execPopulate();
+        
+                    return res.status(200).json({
+                        data: {
+                            comment: comment
+                        },
+                        message: "Post created!"
+                    });
+                }
+    
+    
+                req.flash('success', 'Comment published!');
+    
                 res.redirect('/');
-        }
-            }catch(err){
-                console.log('Error',err);
-                return;
             }
+        }catch(err){
+            req.flash('error', err);
+            return;
         }
+        
+    }
 
 module.exports.destroy = async function(req,res){
     try{
@@ -29,14 +46,31 @@ module.exports.destroy = async function(req,res){
             comment.remove();
 
            let post = await Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}});
-                return res.redirect('back');
-        }else{
-            return res.redirect('back');
-        }
-    }catch(err){
-        console.log('Error',err);
-        return;
+
+
+     // send the comment id which was deleted back to the views
+     if (req.xhr){
+        return res.status(200).json({
+            data: {
+                comment_id: req.params.id
+            },
+            message: "Post deleted"
+        });
     }
+
+
+    req.flash('success', 'Comment deleted!');
+
+    return res.redirect('back');
+}else{
+    req.flash('error', 'Unauthorized');
+    return res.redirect('back');
+}
+}catch(err){
+req.flash('error', err);
+return;
+}
+
 }
 
 
