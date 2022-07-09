0484562e8b1d114870175732e1dc29c57364be07
Sonu825313 <sonukumarraj1341998@gmail.com>
2022-07-09 10:31:54 +0530

    Authentication with APIS:: JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const extractJWT = require('passport-jwt').ExtractJwt;
+
+const user = require('../models/user');
+
+let opts = {
+    jwtFromRequest : extractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey : 'codeial'
+}
+
+passport.use(new JWTStrategy(opts, function(jwtPayload, done){
+  
+    User.findById(jwtPayload._id, function(err, user){
+        if(err){console.log('Error in finding user from jwt'); return;}
+
+        if(user){
+            return done(null, user);
+        }else{
+            return done(null, false);
+        }
+    })
+}));
+
+module.exports = passport;
+
