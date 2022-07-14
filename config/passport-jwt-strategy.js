7c4e37fb6315007f888d3c3997c74aec8889b179
Sonu825313 <sonukumarraj1341998@gmail.com>
2022-07-14 09:26:48 +0530

    APIs (making APIs and JWT authentication)

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -2,16 +2,16 @@ const passport = require('passport');
 const JWTStrategy = require('passport-jwt').Strategy;
 const extractJWT = require('passport-jwt').ExtractJwt;
 
-const user = require('../models/user');
+const User = require('../models/user');
 
 let opts = {
     jwtFromRequest : extractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey : 'codeial'
 }
 
-passport.use(new JWTStrategy(opts, function(jwtPayload, done){
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
   
-    User.findById(jwtPayload._id, function(err, user){
+    User.findById(jwtPayLoad._id, function(err, user){
         if(err){console.log('Error in finding user from jwt'); return;}
 
         if(user){
--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -13,7 +13,7 @@ module.exports.createSession = async function(req, res){
         });
      }
 
-     return res.json(200, {
+     return res.status(200).json({
         message: 'Sign in successful, here is your token, please keep it safe',
         data: {
             token: jwt.sign(user.toJSON(), 'codeial', {expiresIn: '100000'})
